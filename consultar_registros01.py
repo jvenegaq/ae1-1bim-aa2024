76b82e727010af2654a270bec8d7ae46619dbf16
jvenegaq <jevenegas1@utpl.edu.ec>
2024-05-31 23:55:57 -0500

    actualizacion bd

--- consultar_registros01.py
+++ consultar_registros01.py
@@ -6,7 +6,7 @@ try:
     client = pymongo.MongoClient('mongodb://localhost:27017/')
     db = client['mi_base_de_datos']  
 
-    # Obtener todas las entradas de las colecciones LocalesComida y CentrosDeportivos
+    # Entradas de las colecciones LocalesComida y CentrosDeportivos
     locales_comida = db['LocalesComida'].find()
     centros_deportivos = db['CentrosDeportivos'].find()
 
@@ -16,12 +16,12 @@ except pymongo.errors.ConnectionFailure as e:
 except Exception as e:
     print(f"Error inesperado: {e}")
 
-# Iterar y presentar la información de cada documento en LocalesComida
+# Presenta la información de cada documento en LocalesComida
 print("Locales Comida:")
 for local in locales_comida:
     print(local)
 
-# Iterar y presentar la información de cada documento en CentrosDeportivos
+# Presenta la información de cada documento en CentrosDeportivos
 print("Centros Deportivos:")
 for centro in centros_deportivos:
     print(centro)
--- consultar_registros03.py
+++ consultar_registros03.py
@@ -3,7 +3,7 @@ import pymongo
 # Conectar a MongoDB local
 try:
     client = pymongo.MongoClient('mongodb://localhost:27017/')
-    db = client['mi_base_de_datos']  # Reemplaza con el nombre de tu base de datos
+    db = client['mi_base_de_datos']  
 
     print("Conexión a la base de datos establecida correctamente.")
 except pymongo.errors.ConnectionFailure as e:
@@ -11,19 +11,19 @@ except pymongo.errors.ConnectionFailure as e:
 except Exception as e:
     print(f"Error inesperado: {e}")
 
-# Obtener y ordenar todos los registros de la colección LocalesComida por calificación
+# Registros de la colección LocalesComida por calificación
 lista_LocalesComida = db['LocalesComida'].find().sort("calificacion", pymongo.ASCENDING)
 
-# Obtener y ordenar todos los registros de la colección CentrosDeportivos por costo de membresía
+# Registros de la colección CentrosDeportivos por costo de membresía
 lista_CentrosDeportivos = db['CentrosDeportivos'].find().sort("costo_membresia", pymongo.ASCENDING)
 
-# Iterar y presentar la información de cada documento en LocalesComida
+# Presenta la información de cada documento en LocalesComida
 print("Locales de Comida:")
 for localescomida in lista_LocalesComida:
     print(f"Nombre: {localescomida['nombre']}")
     print(f"Calificación: {localescomida['calificacion']}")
 
-# Iterar y presentar la información de cada documento en CentrosDeportivos
+# Presentar la información de cada documento en CentrosDeportivos
 print("\nCentros Deportivos:")
 for centrosdeportivos in lista_CentrosDeportivos:
     print(f"Nombre: {centrosdeportivos['nombre']}")
